b4538d3f382ee62aa7c087d5d5ca836fe9c0b2e1
pl1016814 <pl1016814@ahschool.com>
2026-01-06 09:45:31 -0500

    Comment out image processing code
    Comment out the image processing code in main.py.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import cv2
-import numpy as np
-
-image = pass
-kabba = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
-kabba = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, 20)
-kabba = kabba.astype(np.float32)
-cv.circle(kabba, (kabba[0], kabba[1]), kabba[2], (255,0,0), 2)
+# import cv2
+# import numpy as np
+# imagePath = "./image.png"
+# jabba = cv2.imread(imagePath, 0)
+# #jabba = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
+# kabba = cv2.HoughCircles(jabba, cv2.HOUGH_GRADIENT, 1, 20)
+# print(kabba)
+# kabba = kabba.astype(np.float32)
+# cv2.circle(kabba, (kabba[0], kabba[1]), kabba[2], (255,0,0), 2)
+# cv2.imshow("Display", jabba)
+# cv2.waitKey(0)
