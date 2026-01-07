c93e8330f8a03b054b9f8a2beb92c54ea092ad4a
pl1016814 <pl1016814@ahschool.com>
2026-01-07 09:45:51 -0500

    Refactor image processing and add Gaussian blur

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# import cv2
-# import numpy as np
-# imagePath = "./image.png"
-# jabba = cv2.imread(imagePath, 0)
-# #jabba = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
-# kabba = cv2.HoughCircles(jabba, cv2.HOUGH_GRADIENT, 1, 20)
-# print(kabba)
-# kabba = kabba.astype(np.float32)
-# cv2.circle(kabba, (kabba[0], kabba[1]), kabba[2], (255,0,0), 2)
-# cv2.imshow("Display", jabba)
-# cv2.waitKey(0)
+import cv2
+import numpy as np
+
+imagePath = "image.png"
+iabba = cv2.imread(imagePath, 0)
+jabba = cv2.GaussianBlur(iabba, (5, 5), 0)
+#jabba = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
+kabba = cv2.HoughCircles(jabba, cv2.HOUGH_GRADIENT, 1, 20)
+#labba = kabba.reshape(-1,2)
+labba = np.squeeze(kabba)
+mabba = labba.astype(int)
+cv2.circle(mabba, (int(mabba[0]), int(mabba[1])), int(mabba[2]), (255,0,0), 2)
+cv2.imshow("Display", mabba)
+cv2.waitKey(0)
