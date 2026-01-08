1450d5a1982da1eb9a5f46c029807440867b3358
pl1016814 <pl1016814@ahschool.com>
2026-01-08 09:31:30 -0500

    Refactor circle detection using OpenCV

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import cv2
 import numpy as np
+import cv2 as cv
 
-imagePath = "image.png"
-iabba = cv2.imread(imagePath, 0)
-jabba = cv2.GaussianBlur(iabba, (5, 5), 0)
-#jabba = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
-kabba = cv2.HoughCircles(jabba, cv2.HOUGH_GRADIENT, dp = 1, minDist = 200, param1 = 120, param2 = 100)
-#labba = kabba.reshape(-1,2)
-labba = np.squeeze(kabba)
-#labba = kabba.reshape(1, 3)
-mabba = labba.astype(int)
-cv2.circle(mabba, (int(mabba[0]), int(mabba[1])), int(mabba[2]), (255,0,0), 2)
-cv2.imshow("Display", mabba)
-cv2.waitKey(0)
+img = cv.imread('image.png', cv.IMREAD_GRAYSCALE)
+img = cv.GaussianBlur(img, (5,5), 0)
+cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
 
+circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1.33, 200,
+                          param1=120, param2=100, minRadius=800, maxRadius=2000)
 
+circles = np.uint16(np.around(circles))
+for i in circles[0, :]:
+    # draw the outer circle
+    cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    # draw the center of the circle
+    cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cv.imshow('detected circles', cimg)
+cv.waitKey(0)
+cv.destroyAllWindows()
