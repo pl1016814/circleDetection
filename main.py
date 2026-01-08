584c615868801b741f58e977e6e1b322b0335893
pl1016814 <pl1016814@ahschool.com>
2026-01-08 00:11:48 -0500

    Update HoughCircles parameters and reshape logic

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ imagePath = "image.png"
 iabba = cv2.imread(imagePath, 0)
 jabba = cv2.GaussianBlur(iabba, (5, 5), 0)
 #jabba = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
-kabba = cv2.HoughCircles(jabba, cv2.HOUGH_GRADIENT, 1, 20)
+kabba = cv2.HoughCircles(jabba, cv2.HOUGH_GRADIENT, dp = 1, minDist = 200, param1 = 120, param2 = 100)
 #labba = kabba.reshape(-1,2)
 labba = np.squeeze(kabba)
+#labba = kabba.reshape(1, 3)
 mabba = labba.astype(int)
 cv2.circle(mabba, (int(mabba[0]), int(mabba[1])), int(mabba[2]), (255,0,0), 2)
 cv2.imshow("Display", mabba)
 cv2.waitKey(0)
+
+
