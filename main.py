92de78ae54a1c60757c64b53f84c8d8141ded273
pl1016814 <pl1016814@ahschool.com>
2026-01-09 16:04:53 -0500

    Update circular shape detection logic in main.py
    Refactor circular shape detection process to improve image handling and circle detection.

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
+#"Circular Shape Detection" - Constantine Munoz P2, V1.0.4
+#Process of code:
+#Import libraries -> resize image to reduce lag -> grayscale + gaussian blur ->
+#Run Hough -> Find largest out of circles -> Draw the actual circle -> Show entire image
 
-import numpy as np
 import cv2 as cv
+import numpy as np
 
-img = cv.imread('image.png', cv.IMREAD_GRAYSCALE)
-img = cv.GaussianBlur(img, (5,5), 0)
-cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+# SETUP
+img = cv.imread("image.png")
+img = cv.resize(img, (375, 500), interpolation=cv.INTER_AREA)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#gaussian blur as stated in documentation doesn't work for my soda can, idk why
+gray = cv.GaussianBlur(gray, (7, 7), 1.5)
 
+circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, dp=1.2, minDist=200, param1=160, param2=35, minRadius=100, maxRadius=200)
+#opencv has a list in a list, this takes the circle that has the highest radius
+x, y, r = circles[0][circles[0][:, 2].argmax()]
 
+#they come back as floats
+cv.circle(img, (int(x), int(y)), int(r), (0, 255, 0), 3)
+cv.circle(img, (int(x), int(y)), 2, (0, 255, 0), 3)
 
-
-
-circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 0.75, 200,
-                          param1=100, param2=50, minRadius=800, maxRadius=2000)
-
-
-#circles = int(np.around(circles))
-try:
-    for i in circles[0, :]:
-        circles = np.uint16(np.around(circles))
-        # draw the outer circle
-        cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        # draw the center of the circle
-        cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-except:
-    print('No circles detected')
-
-cv.imshow('detected circles', cimg)
+cv.imshow("Circular Shape Detection", img)
 cv.waitKey(0)
-cv.destroyAllWindows()
